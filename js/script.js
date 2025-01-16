139559e40f33dc34fb829bb8c299fecf78db32e5
k0robeynik <IrinaKorobeynik@coherentsolutions.com>
2025-01-16 19:21:47 +0300

    added show page

--- js/script.js
+++ js/script.js
@@ -94,6 +94,7 @@ async function displaymovieDetails() {
   document.querySelector('#movie-details').appendChild(div);
 };
 
+
 function  displayBackgroundImage(type, path) {
   const overlayDiv = document.createElement('div');
   overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${path})`;
@@ -112,14 +113,14 @@ function  displayBackgroundImage(type, path) {
   } else {
     document.querySelector('#show-details').appendChild(overlayDiv);
   }
-}
+};
 
 async function displayePopularShows() {
     const { results }  = await fetchAPIData('tv/popular');
     results.forEach(show => {
         const showDiv = document.createElement('div');
         showDiv.classList.add('card');
-        showDiv.innerHTML = `<a href="movie-details.html?id=${show.id}">
+        showDiv.innerHTML = `<a href="tv-details.html?id=${show.id}">
             ${
             show.poster_path? `<img
               src="https://image.tmdb.org/t/p/w500${show.poster_path}"
@@ -142,6 +143,61 @@ async function displayePopularShows() {
         document.querySelector('#popular-shows').appendChild(showDiv);
     });
     
+};
+
+async function displayShowDetails() {
+  const seriesId = window.location.search.split('=')[1];
+  console.log(seriesId);
+  const show = await fetchAPIData(`tv/${seriesId}`);
+  displayBackgroundImage('show', show.backdrop_path);
+  const div = document.createElement('div');
+  div.innerHTML = `<div class="details-top">
+          <div>
+             ${
+            show.poster_path? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`:
+            `<img
+              src="../images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+            }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${show.first_air_date}</p>
+            <p>
+              ${show.overview} 
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+        ${show.genres.map((genre)=> `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes:</span>${show.number_of_episodes}</li>
+            <li>
+              <span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air.air_date
+}
+            </li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${show.production_companies.map((prod)=> `<span>${prod.name}</span>`).join(', ')}</div>
+        </div>`;
+  
+  document.querySelector('#show-details').appendChild(div);
 };
 
 // Fetch data from TMDB API
@@ -194,14 +250,15 @@ function init() {
         displaymovieDetails();
             break;
         case '/tv-details.html':
-            console.log('show details');
+        console.log('show details');
+        displayShowDetails();
             break;
         case '/search.html':
             console.log('search');
             break;
 
     }
-    highlightLink();   
+  //  highlightLink();   
 }
 
 document.addEventListener('DOMContentLoaded', init);
