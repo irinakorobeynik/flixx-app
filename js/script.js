65d67a8d8bc7345ef2ba94c4e42b31708209b169
k0robeynik <IrinaKorobeynik@coherentsolutions.com>
2025-01-16 17:46:53 +0300

    added background to movie page

--- js/script.js
+++ js/script.js
@@ -45,6 +45,7 @@ async function displayePopularMovies() {
 async function displaymovieDetails() {
   const movieId = window.location.search.split('=')[1];
   const movie = await fetchAPIData(`movie/${movieId}`);
+  displayBackgroundImage('movie', movie.backdrop_path);
   const div = document.createElement('div');
   div.innerHTML = `<div class="details-top">
           <div>
@@ -92,6 +93,26 @@ async function displaymovieDetails() {
   
   document.querySelector('#movie-details').appendChild(div);
 };
+
+function  displayBackgroundImage(type, path) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${path})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
+}
 
 async function displayePopularShows() {
     const { results }  = await fetchAPIData('tv/popular');
