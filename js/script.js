3b94dbaa621c047d616cf21333dd467b4d7dfdf7
k0robeynik <IrinaKorobeynik@coherentsolutions.com>
2025-01-16 17:32:55 +0300

    added movie details page

--- js/script.js
+++ js/script.js
@@ -43,9 +43,55 @@ async function displayePopularMovies() {
 };
 
 async function displaymovieDetails() {
-  const movieId = window.location.search.split('=');
-  console.log(movieId[1]);
-}
+  const movieId = window.location.search.split('=')[1];
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  const div = document.createElement('div');
+  div.innerHTML = `<div class="details-top">
+          <div>
+            ${
+            movie.poster_path? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`:
+            `<img
+              src="../images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+            }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)}/10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+             ${movie.overview} 
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+            ${movie.genres.map((genre)=> `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${addCommasToNumber(movie.runtime)} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies.map((prod)=> `<span>${prod.name}</span>`).join(', ')}</div>
+        </div>`;
+  
+  document.querySelector('#movie-details').appendChild(div);
+};
 
 async function displayePopularShows() {
     const { results }  = await fetchAPIData('tv/popular');
@@ -101,6 +147,14 @@ function highlightLink() {
         }
     });   
 };
+
+function addCommasToNumber(number) {
+      number = number.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(number))
+    number = number.replace(pattern, "$1,$2");
+    return number;
+}
 
 // init App
 function init() {
